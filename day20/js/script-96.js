ec7bbfaaf5fc4ae0ce089031466e9d560f1daad4
duyguyilmaz1 <duy.yilmz@gmail.com>
2024-02-24 22:32:40 +0300

    day20-day21

--- day20/js/script-96.js
+++ day20/js/script-96.js
@@ -5,14 +5,17 @@ import {
     numericSortDESC,
     randomSort
 } from "./sorting.js";
+
 const arrStrings = ["Ali", "Ayşe", "Veli", "Cengiz", "Aysun", "Ayten", "Faruk"];
 const arrNumbers = [23, 45, 1, 34, 115, 1134, 47, 33];
+
 const btnSort1 = document.getElementById("btnSort1");
 const btnSort2 = document.getElementById("btnSort2");
 const btnSort3 = document.getElementById("btnSort3");
 const btnSort4 = document.getElementById("btnSort4");
 const btnSort5 = document.getElementById("btnSort5");
 const result = document.getElementById("result");
+
 btnSort1.addEventListener("click", () => {
     result.textContent = stringSortASC(arrStrings);
 });
